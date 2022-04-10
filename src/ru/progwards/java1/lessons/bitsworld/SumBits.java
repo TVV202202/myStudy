c3f904e0f21d9392a86680a3f66c5682aa18ea97
VT <victor.v.tarasov@yandex.ru>
2022-04-10 20:08:39 +0300

    1

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -1,17 +1,25 @@
 package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
-    public static int sumBits(byte value){
-        int result=0;
-        while (value>0) {
+    public static int sumBits(byte value) {
+        int result = 0;
+        boolean flag=false;
+        if (value < 0) {
+            flag =true;
+            value = (byte) ~value;
+        }
+        while (value > 0) {
             int tmp = value & 1;
-            value >>>= 1;
+            value >>= 1;
             result += tmp;
+        }
+        if (flag) {
+            result = 8 - result;
         }
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumBits((byte) 0b0100101));
+        System.out.println(sumBits((byte) 0b10000000));
     }
 }
