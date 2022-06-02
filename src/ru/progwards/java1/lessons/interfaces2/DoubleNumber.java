f79db3782c8fb7f664262846e6330f53f5a800b2
VT <victor.v.tarasov@yandex.ru>
2022-06-02 15:10:19 +0300

    A7.1

---
+++ src/ru/progwards/java1/lessons/interfaces2/DoubleNumber.java
@@ -0,0 +1,43 @@
+package ru.progwards.java1.lessons.interfaces2;
+
+public class DoubleNumber extends Number {
+    double num;
+
+    public DoubleNumber(double num) {
+        this.num = num;
+    }
+
+    @Override
+    public Number mul(Number num) {
+        return new DoubleNumber(this.num * Double.parseDouble(num.toString()));
+    }
+
+    @Override
+    public Number div(Number num) {
+        return new DoubleNumber(this.num / Double.parseDouble(num.toString()));
+    }
+
+    @Override
+    public Number newNumber(String strNum) {
+        return new DoubleNumber(Double.parseDouble(strNum));
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(num);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return Double.compare(this.num, Double.parseDouble(o.toString()));
+    }
+
+    public static void main(String[] args) {
+        Number test1 = new DoubleNumber(2.5);
+        Number test2 = new DoubleNumber(2.0);
+//        System.out.println(test1.mul(test2));
+//        System.out.println(test1.div(test2));
+//        System.out.println(test1.newNumber("4.865488576867"));
+        System.out.println(test1.compareTo(test2));
+    }
+}
